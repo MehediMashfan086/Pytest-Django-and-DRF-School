89f071829ddf8c8bfc8ff1d20fede231173a34ff
MehediMashfan086 <mehedihasan62086@gmail.com>
2022-04-10 13:45:50 +0600

    2nd commit

--- classroom/tests.py
+++ classroom/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
 class TestStudentModel(TestCase):
+    
     def test_add_a_plus_b(self):
         a = 2
         b = 3
@@ -9,3 +11,56 @@ class TestStudentModel(TestCase):
         c = a + b
         
         self.assertEqual(c, 5)
+    
+    def test_student_can_be_created(self):
+        student_1 = Student.objects.create(
+            first_name="Mehedi",
+            last_name="Hasan",
+            admission_number=101)
+        
+        student_result = Student.objects.last()  #getting the last student
+        
+        self.assertEqual(student_result.first_name, "Mehedi")
+        
+    def test_str_return(self):
+        student_1 = Student.objects.create(
+            first_name="Mehedi",
+            last_name="Hasan",
+            admission_number=101)
+        
+        student_result = Student.objects.last()  #getting the last student
+        
+        self.assertEqual(str(student_result), "Mehedi")
+        
+    def test_fail(self):
+        student_1 = Student.objects.create(
+            first_name="Mehedi",
+            last_name="Hasan",
+            admission_number=101,
+            average_score= 35)
+        
+        student_result = Student.objects.last()  #getting the last student
+        
+        self.assertEqual(student_result.get_grade(), "Fail")
+        
+    def test_pass(self):
+        student_1 = Student.objects.create(
+            first_name="Mehedi",
+            last_name="Hasan",
+            admission_number=101,
+            average_score= 60)
+        
+        student_result = Student.objects.last()  #getting the last student
+        
+        self.assertEqual(student_result.get_grade(), "Pass")
+        
+    def test_excillent(self):
+        student_1 = Student.objects.create(
+            first_name="Mehedi",
+            last_name="Hasan",
+            admission_number=101,
+            average_score= 80)
+        
+        student_result = Student.objects.last()  #getting the last student
+        
+        self.assertEqual(student_result.get_grade(), "Excellent")
